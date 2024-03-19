419b5f09bdeed52eda89e0c2dc7ed057fb329f4b
INTESARSHAWON <intesarshawon@gmail.com>
2024-03-19 19:28:08 +0600

    Unary Operator

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ console.log ("x/y=", x/y);
 console.log ("x%y=", x%y);
 console.log ("x**y=", x**y);
 
+//Unary operator
+
+let a=6;
+let b=3;
+let c=5;
+let d=6;
+
+// ++a pre increament a++ post increament
+
+++a; //7
+console.log ("++a=", ++a); //arek bere 8 hoise
+
+b++; //3 e ache
+console.log ("b++=", b++); //ebar 4 hoise
+
+--c; //4 hoise
+console.log ("--c=", --c); //3 hoye geche
+
+d--; //6 e ace
+console.log ("d--=", d--); //5 hoye geche
