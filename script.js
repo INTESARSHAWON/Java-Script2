532264f3fe455c7778d7020b74f68a38643a3d6d
INTESARSHAWON <intesarshawon@gmail.com>
2024-03-19 19:25:00 +0600

    Operators

---
+++ script.js
@@ -0,0 +1,12 @@
+//Arithmatic operator
+
+let x=5;
+let y=5;
+
+console.log ("x+y=", x+y);
+console.log ("x-y=", x-y);
+console.log ("x*y=", x*y);
+console.log ("x/y=", x/y);
+console.log ("x%y=", x%y);
+console.log ("x**y=", x**y);
+
